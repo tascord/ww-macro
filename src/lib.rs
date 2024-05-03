c603719fef4dc655fe175287207325662325529d
Flora Hill <imflo.pink@gmail.com>
2024-05-03 01:12:19 +0000

    Add where clause

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use syn::{
-    parse::{Parse, ParseStream}, parse_macro_input, Block, Generics, Ident, Token, Type, Visibility
+    parse::{Parse, ParseStream}, parse_macro_input, Block, Generics, Ident, Token, Type, Visibility, WhereClause
 };
 
 struct Meta(
@@ -10,6 +10,7 @@ struct Meta(
     pub Type,
     pub Ident,
     pub Type,
+    pub Option<WhereClause>,
     pub Block,
 );
 
@@ -34,9 +35,11 @@ impl Parse for Meta {
 
         let _ = input.parse::<Token![->]>()?;
         let ret = input.parse::<Type>()?;
+        let clause = input.parse::<WhereClause>().ok();
+
         let block = input.parse::<Block>()?;
 
-        Ok(Meta(vis, name, generics, arg, arg_name, ret, block))
+        Ok(Meta(vis, name, generics, arg, arg_name, ret, clause, block))
     }
 }
 
@@ -47,14 +50,14 @@ pub fn worked(attr: TokenStream, item: TokenStream) -> TokenStream {
     let attr = parse_macro_input!(attr as syn::LitStr);
     let path = attr.value();
 
-    let (vis, name, generics, arg, arg_name, ret, block) =
-        (meta.0, meta.1, meta.2, meta.3, meta.4, meta.5, meta.6);
+    let (vis, name, generics, arg, arg_name, ret, clause, block) =
+        (meta.0, meta.1, meta.2, meta.3, meta.4, meta.5, meta.6, meta.7);
     let quiet_name = quote::format_ident!("__{}", name.to_string());
     let quiet_name_string = quiet_name.to_string();
 
     quote::quote! {
         #[wasm_bindgen]
-        pub fn #quiet_name #generics(i__: Vec<u8>) -> js_sys::Array {
+        pub fn #quiet_name #generics(i__: Vec<u8>) -> js_sys::Array #clause {
             let #arg_name: #arg = bincode::decode_from_slice(&i__, bincode::config::standard()).unwrap().0;
             let res = #block;
             
@@ -66,7 +69,7 @@ pub fn worked(attr: TokenStream, item: TokenStream) -> TokenStream {
             array
         }
 
-        #vis async fn #name #generics(i: #arg, c: impl Fn(#ret) + 'static) {
+        #vis async fn #name #generics(i: #arg, c: impl Fn(#ret) + 'static) #clause  {
             let w = WrappedWorker::<#arg, #ret>::new(#path).await;
             w.run_task(#quiet_name_string, i, c);
         }
